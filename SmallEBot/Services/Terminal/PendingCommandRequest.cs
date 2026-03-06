5f8b65b4d84d1f2fd91a765d31473542f202990b
palink <1006282023@qq.com>
2026-03-06 20:12:14 +0800

    refactor: delete obsolete command confirmation files
    - Remove CommandConfirmationService.cs
    - Remove ICommandConfirmationService.cs
    - Remove PendingCommandRequest.cs
    - Remove PendingRequestEventArgs.cs
    - Remove PendingRequestCompletedEventArgs.cs
    - Remove CommandConfirmationContext.cs
    - Remove ICommandConfirmationContext.cs
    - Remove CommandConfirmationStrip.razor

--- SmallEBot/Services/Terminal/PendingCommandRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace SmallEBot.Services.Terminal;
-
-/// <summary>Represents a command pending user approval. Used by the confirmation strip UI.</summary>
-public sealed class PendingCommandRequest
-{
-    public string RequestId { get; init; } = "";
-    public string Command { get; init; } = "";
-    public string? WorkingDirectory { get; init; }
-}
--- SmallEBot/Services/Terminal/PendingRequestEventArgs.cs
+++
@@ -1,8 +0,0 @@
-namespace SmallEBot.Services.Terminal;
-
-/// <summary>EventArgs for when a new command confirmation request is added.</summary>
-public sealed class PendingRequestEventArgs : EventArgs
-{
-    public string ContextId { get; init; } = "";
-    public PendingCommandRequest Request { get; init; } = null!;
-}
