b742606f4ffc2f38a09f8e9e415809d8e4846380
palink <1006282023@qq.com>
2026-03-07 19:16:13 +0800

    refactor(domain): remove unused ValueObject base class

--- SmallEBot.Domain/Common/ValueObject.cs
+++
@@ -1,8 +0,0 @@
-// SmallEBot.Domain/Common/ValueObject.cs
-namespace SmallEBot.Domain.Common;
-
-/// <summary>
-/// Base class for value objects in DDD.
-/// Value objects are immutable and compared by value.
-/// </summary>
-public abstract record ValueObject;
